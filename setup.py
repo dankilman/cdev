83a2e578eab44b43781a210bd211f57433b86e23
Pavel Brodsky <mcouthon@gmail.com>
2017-01-31 16:32:06 +0200

    CFY-6377 Fix clue's blueprint defaults and update clash version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         'sh',
         'virtualenvwrapper',
-        'clash==0.16'
+        'clash==0.17'
     ],
     include_package_data=True,
     entry_points={
